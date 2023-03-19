7857a5fa3fd394f2a1acd681673650ddb9147d18
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2023-03-19 15:14:29 +0330

    add ApiResource

--- IdentityServerPractice/Program.cs
+++ IdentityServerPractice/Program.cs
@@ -1,3 +1,7 @@
+using IdentityServer4.Models;
+
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddIdentityServer().AddDeveloperSigningCredential();
+builder.Services.AddIdentityServer().AddDeveloperSigningCredential()
+    .AddInMemoryApiResources(new List<ApiResource>()).
+    AddInMemoryClients(new List<Client>());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
